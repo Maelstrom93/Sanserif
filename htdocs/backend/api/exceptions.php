331c339b4152fb1517dfa9ddf331a6eebc7ac553
Maelstrom93 <togethercrunchy@gmail.com>
2025-12-15 18:10:06 +0100

    Update and rename exceptions to exceptions.php

---
+++ htdocs/backend/api/exceptions.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+class ApiException extends RuntimeException {}
+class DbException extends RuntimeException {}
+class BadRequestException extends RuntimeException {}
+class ValidationException extends RuntimeException {}
+class NotFoundException extends RuntimeException {}
+class ConflictException extends RuntimeException {}
+class OperationFailedException extends RuntimeException {}
+
+?>
